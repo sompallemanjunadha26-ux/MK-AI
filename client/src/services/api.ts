03929e3f737dddf56ff226221adaca3ca44bd54b
manjunadha2518 <rsmanjunadha@gmail.com>
2026-02-25 00:42:43 +0530

    force production api

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    "https://mk-ai-74xu.onrender.com",
 });
 
 export default API;
